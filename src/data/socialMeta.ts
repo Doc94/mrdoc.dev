52955f05d38d4b426415e6fffb630b0a4e00539f
Doc <nachito94@msn.com>
2025-05-12 12:07:32 -0400

    feat: release files to view

---
+++ src/data/socialMeta.ts
@@ -0,0 +1,13 @@
+export const tag = "@doc";
+export const author = "Doc";
+
+export const defaultMeta = {
+  title: "Doc - Inicio",
+  description:
+    "Discover the portfolio of Pedro (Doc)",
+  keywords: ["pedro", "doc", "software developer", "programming", "portfolio", "chile", "java", "minecraft"],
+  ogImage: {
+    src: "/banner.png",
+    alt: "Doc - Inicio",
+  },
+};
